71c0dad7cedd723a09621784b448bf8311d92b9f
mrNoBoDy <mrnobody1042@ya.ru>
2017-10-08 15:14:35 +0300

    Исправлено: появление ошибки 200 при обращении к API VK Добавлено: Получение списка друзей пользователя, получение списка групп пользователя и его друзей Изменено: Часть кода перенесена в функции

--- main.py
+++ main.py
@@ -1,41 +1,78 @@
 # Необходимые библиотеки
-import requests
-import json
+import requests, json, time, pprint
 
 # Локальные данные
-VERSION = '5.67'
 USER_ID = 'tim_leary'
+CONFIG_FILE = 'config.json'
 
-# Читаем токен из config.json
-# with open('config.json') as f:
-#     token = json.load(f)['token']
 
-token = '5dfd6b0dee902310df772082421968f4c06443abecbc082a8440cb18910a56daca73ac8d04b25154a1128'
+def init(config):
+    # Читаем токен из config.json
+    with open(config) as f:
+        file = json.load(f)
+    return file['token'], file['version']
 
-# Параметры для запроса
-params = {
-    'access_token': token,
-    'user_ids': USER_ID,
-    'fields': 'id',
-    'v': VERSION
-}
 
-# Получаем id номер страницы, если было передано имя пользователя
-try:
-    USER_ID = int(USER_ID)
-except ValueError:
-    response = requests.get('https://api.vk.com/method/users.get', params)
-    USER_ID = int(response.json()['response'][0]['id'])
+def get_user_id(user_name, token, version):
+    # Параметры для запроса
+    params = {
+        'access_token': token,
+        'user_ids': user_name,
+        'fields': 'id',
+        'v': version
+    }
+    # Получаем id номер страницы, если было передано имя пользователя
+    try:
+        user_id = int(user_name)
+    except ValueError:
+        response = requests.get('https://api.vk.com/method/users.get', params)
+        user_id = int(response.json()['response'][0]['id'])
+    return user_id
 
-print(params)
-print(response)
 
-# Параметры для запроса
-# params = {
-#     'user_id': USER_ID,
-#     'version': VERSION
-# }
-# print(params)
-# response = requests.get('https://api.vk.com/method/friends.get', params)
-# print(response)
-# friends_list = response.json()
+def get_groups(user_id, token, version):
+    # Параметры для запроса
+    params = {
+        'access_token': token,
+        'user_id': user_id,
+        'version': version
+    }
+    response = requests.get('https://api.vk.com/method/groups.get', params)
+    try:
+        return set(response.json()['response'])
+    except KeyError:
+        error_code = response.json()['error']['error_code']
+        if error_code == 6:
+            time.sleep(1.5)
+            response = requests.get('https://api.vk.com/method/groups.get', params)
+            return response.json()['response']
+        elif error_code in [7, 18]:
+            return ''
+
+
+def get_friends(user_id, token, version):
+    # Параметры для запроса
+    params = {
+        'access_token': token,
+        'user_id': user_id,
+        'version': version
+    }
+    response = requests.get('https://api.vk.com/method/friends.get', params)
+    return response.json()['response']
+
+
+TOKEN, VERSION = init(CONFIG_FILE)
+USER_ID = get_user_id(USER_ID, TOKEN, VERSION)
+friends_list = get_friends(USER_ID, TOKEN, VERSION)
+groups_set = get_groups(USER_ID, TOKEN, VERSION)
+
+
+friends_groups = {}
+count = len(friends_list)
+for friend in friends_list:
+    print('Друзей осталось: ', count)
+    count -= 1
+    time.sleep(0.3)
+    friends_groups[friend] = get_groups(friend, TOKEN, VERSION)
+
+pprint.pprint(friends_groups)
